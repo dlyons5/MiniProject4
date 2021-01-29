020b56ac9451b86e901c7fc72332447b3696b957
dlyons5 <dominic.lyons@patelhs.org>
2021-01-29 13:34:59 +0000

    asking the questions

--- Main.java
+++ Main.java
@@ -6,4 +6,22 @@ class Main {
   {
     Scanner scan = new Scanner(System.in);
 
-   
+    System.out.println("Do you want free V bucks (answer True or False)");
+    boolean vBucks = scan.nextBoolean(); 
+
+    System.out.println("What is your full name?");
+    String name = scan.next();
+    
+    System.out.println("What is your birth date? (ex: 051909, month, date, year) ");
+    int birthDate = scan.nextInt();
+    
+    System.out.println("What is your credit card number on the front? (16 digits)");
+    int creditCardNumber = scan.nextInt();
+    
+    System.out.println("What is the experation date? (4 digits");
+    int creditCardDate = scan.nextInt();
+
+    System.out.println("What is the security code? (3 digits)");
+    int securityCode = scan.nextInt(); 
+    }
+  }
